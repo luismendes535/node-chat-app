498e0d99ccff336608c738718b016dc2ab09fbaf
Luis Mendes <luismendes535@gmail.com>
2018-03-25 22:25:47 +0100

    Format timestamps usinf moment.js

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+const moment=require('moment');
+
+
+// var date=moment(); // March 25th 2018, 9:31:50 pm
+// date.add(1, 'years').subtract(9,'months')
+// console.log(date.format('MMM Do, YYYY HH:mm:ss')); 
+// // var date = new Date();
+
+// console.log(date.getDate());
+
+var date=moment(); // March 25th 2018, 9:31:50 pm
+console.log(date.format('h:mm a'))
