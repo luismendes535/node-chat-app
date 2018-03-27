244c1af64c4b901b06aacdd081058a386525b462
Luis Mendes <luismendes535@gmail.com>
2018-03-27 02:59:01 +0100

    Add users class and update userlist event

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,26 @@ const expect = require('expect');
 var {Users} = require('./users');
 
 describe('Users',()=>{
+
+    beforeEach(()=>{
+        users = new Users();
+        users.users = [{
+            id:1,
+            name:'Luis',
+            room: 'Node Course'
+        },
+        {
+            id:2,
+            name:'Miguel',
+            room: 'React Course'
+        },
+        {
+            id:3,
+            name:'João',
+            room: 'Node Course'
+        }];
+    })
+
     it('should add new user', ()=>{
         var users = new Users();
         var user ={
@@ -10,7 +30,44 @@ describe('Users',()=>{
             room:'Office'
         };
         var resUser = users.addUser(user.id, user.name, user.room);
-
         expect(users.users).toEqual(([user]))
     });
+
+    it('should remove a user', ()=>{
+        var userId = 1;        
+        var user = users.removeUser(userId);
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove a user',()=>{
+        var userId = 111111;        
+        var user = users.removeUser(userId);
+        expect(user).not.toBeDefined();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('it should find user',()=>{
+        var userId = 2;
+        var user = users.getUser(userId);
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find user',()=>{
+        
+        var userId = '222';
+        var user = users.getUser(userId);
+        expect(user).not.toBeDefined();
+    });
+    it('should return names for node course',()=>{
+        var userList = users.getUserList('Node Course');
+
+        expect(userList).toEqual(['Luis','João']);
+    })
+    
+    it('should return names for react course',()=>{
+        var userList = users.getUserList('React Course');
+
+        expect(userList).toEqual(['Miguel']);
+    })
 })
